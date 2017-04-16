2f3ecf2ffc597f50c1b63fdec61d76c9e51e94cb
Marcio Valenzuela <Quique123@yahoo.com>
2017-04-16 13:06:01 -0600

    log testing

--- public/sw1_off.py
+++ public/sw1_off.py
@@ -1,6 +1,9 @@
 # Edit line 6 to match your chosen GPIO pin
-
+import logging
 import RPi.GPIO as GPIO
+
+logging.warning('gpio pin toggle!')  # will print a message to the console
+
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.IN)
