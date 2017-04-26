f26fb4a12ab84adbb776a190331c9a2f671c9897
Marcio Valenzuela <Quique123@yahoo.com>
2017-04-25 20:37:35 -0600

    Update sw1_off.py
    added code to interface with bluetooth serial and control tsrb430

--- public/sw1_off.py
+++ public/sw1_off.py
@@ -1,10 +1,34 @@
-# Edit line 6 to match your chosen GPIO pin
+# Edit line 6 to match your chosen GPIO pin-off
 import logging
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 
-logging.warning('gpio pin toggle!')  # will print a message to the console
+logging.warning('gpio pin toggle OFF!')  # will print a message to the console
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(23, GPIO.IN)
-GPIO.cleanup()
+#GPIO.setwarnings(False)
+#GPIO.setmode(GPIO.BCM)
+#GPIO.setup(23, GPIO.IN)
+#GPIO.cleanup()
+
+#!/usr/bin/env python
+
+import serial
+
+ser = serial.Serial(
+        port='/dev/serial0',
+        baudrate=9600,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout=1
+)
+
+print "Serial is open: " + str(ser.isOpen())
+
+print "Now Writing"
+ser.write("o")
+#ser.write("AT+CONNL")
+print "Did write, now read"
+x = ser.readline()
+print "got '" + x + "'"
+
+ser.close()
