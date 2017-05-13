0bc83b393ddf0c74ca5382f302e0852140c3db5f
Marcio Valenzuela <Quique123@yahoo.com>
2017-05-12 22:44:58 -0600

    Create sw4_off.py
    turns relay 4 to position 0, off

---
+++ public/sw4_off.py
@@ -0,0 +1,34 @@
+# Edit line 6 to match your chosen GPIO pin-off
+import logging
+#import RPi.GPIO as GPIO
+
+logging.warning('gpio pin toggle OFF!')  # will print a message to the console
+
+#GPIO.setwarnings(False)
+#GPIO.setmode(GPIO.BCM)
+#GPIO.setup(23, GPIO.IN)
+#GPIO.cleanup()
+
+#!/usr/bin/env python
+
+import serial
+
+ser = serial.Serial(
+        port='/dev/serial0',
+        baudrate=9600,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout=1
+)
+
+print "Serial is open: " + str(ser.isOpen())
+
+print "Now Writing"
+ser.write("r")
+#ser.write("AT+CONNL")
+print "Did write, now read"
+x = ser.readline()
+print "got '" + x + "'"
+
+ser.close()
