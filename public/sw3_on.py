d4a5852fe61e87e41ea2b96d32e487c032b1c6f5
Marcio Valenzuela <Quique123@yahoo.com>
2017-05-12 22:42:44 -0600

    Create sw3_on.py
    turns relay 3 to position 1, on

---
+++ public/sw3_on.py
@@ -0,0 +1,34 @@
+# Edit line 6 to match your chosen GPIO pin-on
+import logging
+#import RPi.GPIO as GPIO
+
+logging.warning('gpio pin toggle!')  # will print a message to the console
+
+#GPIO.setwarnings(False)
+#GPIO.setmode(GPIO.BCM)
+#GPIO.setup(23, GPIO.OUT)
+
+## TEMP CALL togglerelay.py file's code snippet
+#!/usr/bin/env python
+
+import serial
+
+ser = serial.Serial(
+        port='/dev/serial0',
+        baudrate=9600,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout=1
+)
+
+print "Serial is open: " + str(ser.isOpen())
+
+print "Now Writing"
+ser.write("g")
+#ser.write("AT+CONNL")
+print "Did write, now read"
+x = ser.readline()
+print "got '" + x + "'"
+
+ser.close()
